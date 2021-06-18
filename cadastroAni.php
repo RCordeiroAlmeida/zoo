47ea756b16b5be9d1d185adf28a194472cb4b2d9
RCordeiroAlmeida <rafaelcordeiro299@gmail.com>
2021-06-18 12:39:22 -0300

    cadastro o animal

---
+++ cadastroAni.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Animais</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="ani_nome">Nome do animal:</label>
+        <input type="text" name="ani_nome" id="ani_nome">
+
+        <label for="ani_especie">Espécie do animal:</label>
+        <select name="select" name="ani_especie" id="ani_especie">
+            <option value=""></option>
+            <option value=""></option>
+            <option value=""></option>
+        </select>
+
+        <input type="submit" value="Cadastrar">
+    </form>
+</body>
+</html>
